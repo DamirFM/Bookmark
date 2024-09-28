24777349e434e7d92d17bd3c7796a0f5a8d2ea67
Damir Yakupov <yakupovdamir93@gmail.com>
2024-09-27 23:01:49 -0400

    update auth.controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Post, Req } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Request } from 'express';
+import { AuthDto } from './dto';
+
 // Dependency Injection is a design pattern in which a class requests dependencies from external sources rather than creating them itself.
 // Controller will need to call functions from services to perform the business logic.
 // To do that, we need to inject the service into the controller using the @Injectable() decorator. 
@@ -13,13 +15,13 @@ export class AuthController{
     // let's create endpoints for the login and signup methods.
     // As we expect POST for both login and signup, we will use the @Post() decorator.
     @Post('login')
-    login(@Req() req: Request){
+    login(@Body() dto: AuthDto){
         console.log(req.body)
-        return this.authService.login()
+        return this.authService.login(dto)
     }
 
     @Post('signup')
-    signup(){
-        return this.authService.signup()
+    signup(@Body() dto: AuthDto){
+        return this.authService.signup(dto)
     }
 };
